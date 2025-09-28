6c495181f58891ae35ba3b20785fa4d929ddfdf1
fudhla <fudhla24si@mahasiswa.pcr.ac.id>
2025-09-28 15:10:29 +0700

    commit pertama

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    Selamat Datang Di Website Fudhla !!
+    <a href="{{ route('mahasiswa.show') }}">Klik Disini</a> 
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MahasiswaController;
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/sid', function () {
+    return "Ini adalah halaman kelas saya";
+});
+
+Route::get('/nama/{param1}', function ($param1) {
+    return 'Nama saya adalah: '.$param1;
+});
+
+Route::get('/nama/{param1?}', function ($param1= '') {
+    return 'Nama saya adalah: '.$param1; //required
+});
+
+Route::get('/mahasiswa', function () {
+    return 'Halo Mahasiswa'; //required
+})->name('mahasiswa.show');
+
+Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
