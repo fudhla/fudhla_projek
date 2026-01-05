a73806d5c6bdedaa09b66549ce8eb09e58536162
fudhla <fudhla24si@mahasiswa.pcr.ac.id>
2026-01-05 07:16:56 +0700

    tes

---
+++ database/migrations/2026_01_04_182613_add_fasilitas_id_to_peminjaman_fasilitas_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('peminjaman_fasilitas', function (Blueprint $table) {
+        if (!Schema::hasColumn('peminjaman_fasilitas', 'fasilitas_id')) {
+            $table->foreignId('fasilitas_id')
+                ->after('id')
+                ->constrained('fasilitas_umum')
+                ->cascadeOnDelete();
+        }
+    });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+       Schema::table('peminjaman_fasilitas', function (Blueprint $table) {
+        $table->dropForeign(['fasilitas_id']);
+        $table->dropColumn('fasilitas_id');
+    });
+    }
+};
